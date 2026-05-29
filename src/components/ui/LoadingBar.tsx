b4587d3fdba4aa5202986c67b4c443609a5d094d
Ethan-Tillmon7 <etillmon7@gmail.com>
2026-05-29 16:46:00 -0500

    feat: add LoadingBar fixed progress indicator component

---
+++ src/components/ui/LoadingBar.tsx
@@ -0,0 +1,19 @@
+import { useLoadingBarStore } from '../../store/loadingBarStore';
+
+export function LoadingBar() {
+  const active = useLoadingBarStore((s) => s.active);
+  const progress = useLoadingBarStore((s) => s.progress);
+
+  return (
+    <div
+      className="fixed top-0 left-0 z-[100] h-[3px] bg-pm-yellow pointer-events-none"
+      style={{
+        width: `${progress * 100}%`,
+        opacity: active ? 1 : 0,
+        transition: active
+          ? 'width 800ms ease-out, opacity 150ms'
+          : 'width 150ms ease-out, opacity 350ms 150ms',
+      }}
+    />
+  );
+}
